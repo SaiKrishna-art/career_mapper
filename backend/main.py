9f81f5fbcd414778e39677e1e8c769857c63c2eb
Murthy Sai Krishna <126983461+SaiKrishna-art@users.noreply.github.com>
2025-06-04 14:47:37 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
-genai.configure(api_key="AIzaSyAgytqTyc45DbQPlo5-ztbeC7qA63qR2Zg")  # This should be your Bard/Gemini API key
+genai.configure(api_key="")  # This should be your Bard/Gemini API key
 
 app = FastAPI()
 
