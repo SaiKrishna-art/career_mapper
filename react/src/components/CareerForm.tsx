1ffcd2c7c812d9d889f115bf50d750bdbafb9121
Murthy Sai Krishna <126983461+SaiKrishna-art@users.noreply.github.com>
2025-06-06 20:49:31 +0530

    backend deployment link updated.

--- react/src/components/CareerForm.tsx
+++ react/src/components/CareerForm.tsx
@@ -23,7 +23,7 @@ const CareerForm = () => {
     const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
         try {
-            const res = await fetch("http://127.0.0.1:8000/career-mapper", {
+            const res = await fetch("https://career-mapper-6.onrender.com", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
