69b175edaabb665c6c2ff3ca56115fd14628821b
Murthy Sai Krishna <126983461+SaiKrishna-art@users.noreply.github.com>
2025-08-22 16:45:18 +0530

    Update CareerForm.tsx

--- react/src/components/CareerForm.tsx
+++ react/src/components/CareerForm.tsx
@@ -27,7 +27,7 @@ const CareerForm = () => {
         setResponse(""); // Clear previous response
 
         try {
-            const res = await fetch("https://career-mapper-git-fix-branch-saikrishna-arts-projects.vercel.app/career-mapper", {
+            const res = await fetch("https://career-mapper-apmd94vzr-saikrishna-arts-projects.vercel.app/career-mapper", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
